b9b3e86dd24fa64eac7ec0716f1517b876c62349
zzaynAli <bcsf19a022@pucit.edu.pk>
2022-01-06 14:08:48 +0500

    Updates

---
+++ precache-manifest.ecf66ba4fbd8677e0e7cfe97942c0e36.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bd9dc386852f0e62663",
+    "url": "/robofriends/static/css/main.de83b3a0.chunk.css"
+  },
+  {
+    "revision": "1bd9dc386852f0e62663",
+    "url": "/robofriends/static/js/main.1bd9dc38.chunk.js"
+  },
+  {
+    "revision": "0bf54a1201dab9a9b136",
+    "url": "/robofriends/static/js/1.0bf54a12.chunk.js"
+  },
+  {
+    "revision": "d6c1c272cf96c85f66c7",
+    "url": "/robofriends/static/css/2.56b44b6e.chunk.css"
+  },
+  {
+    "revision": "d6c1c272cf96c85f66c7",
+    "url": "/robofriends/static/js/2.d6c1c272.chunk.js"
+  },
+  {
+    "revision": "d9078623073d367ccac9",
+    "url": "/robofriends/static/js/runtime~main.d9078623.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "cb052f4e96803567c05be6928fc3dcdb",
+    "url": "/robofriends/index.html"
+  }
+];
